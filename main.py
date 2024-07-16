06d775945056ef0eaf99e47fd8fe980c4727c31c
PavlaVitaskova <pavlavitaskova@gmail.com>
2024-07-16 13:00:50 +0200

    ověření registrace uživatele

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ registrated_users = {
 table = PrettyTable()
 table.field_names = ["user", "password"]
 
-# Vyplnění řad v tabulce
+# 2.1 Vyplnění řad v tabulce
 for user, password in registrated_users.items():
     table.add_row([user, password])
 
@@ -28,4 +28,10 @@ print(table)
 
 # 3. Přihlášení uživatele
 user_name = input("Zadej své jméno: ")
-user_password = input("Zadej své jméno: ")
+user_password = input("Zadej své heslo: ")
+
+# 4. ověření uživatele
+if registrated_users.get(user_name) == user_password:
+    print(f"Vítej {user_name}! Vyber si z nabídky text k analýze.")
+else:
+    print("Bohužel přihlašovací údaje neodpovídají žádnému registrovanému uživateli. Ukončuji program.")
