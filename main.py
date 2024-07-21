0c75c78ba59bfdaac12e8ca8b55293e337a2e6b8
PavlaVitaskova <pavlavitaskova@gmail.com>
2024-07-21 14:49:50 +0200

    překopání celého kódu po prvním zamítnutí projektu

--- main.py
+++ main.py
@@ -33,19 +33,22 @@ if registrated_users.get(user_name) == user_password:
     print(f"""Welcome to the app, {user_name}.
 We have 3 texts to be analysed."""
     )
-    print(ODDELOVAC)
 
-while True:
-    answer = input("Enter a number btw. 1 and 3 to select: ")
+    while True:
+        answer = input("Enter a number btw. 1 and 3 to select: ")
 
-    if answer.isnumeric() and int(answer) in range(1, 4):
-        break
-    else:
-        print("The value you entered doesn't match the requirements. Try again.")
+        if answer.isnumeric() and int(answer) in range(1, 4):
+            break
+        else:
+            print(
+            "The value you entered doesn't match the requirements. Try again."
+            )
     
-    print(ODDELOVAC)
 else:
     print("Unregistered user, terminating the program.")
+    exit()    
+
+print(ODDELOVAC)
 
 # 5. User selects the text
 selected_text = TEXTS[(int(answer)) - 1]
@@ -53,59 +56,55 @@ selected_text = TEXTS[(int(answer)) - 1]
 # 6. Perform statistics on selected text
 
 # 6.1 Count all words
-text_without_whitespaces = selected_text.strip()
-text_words = text_without_whitespaces.split()
-words_count = len(text_words)
+just_words = selected_text.split()
+cleaned_words = []                  
+# vím, že .strip je to pro účel programu zbytečný krok,
+# ale chci mít čistá slova pro další pokusy s programem
+
+for word in just_words:
+    word = word.strip('.,')
+
+    cleaned_words.append(word)
+
+words_count = len(cleaned_words)
 
 print(f"There are {words_count} words in the selected text.")
 
-# 6.2 Count all words in a string that start with a capital letter
+# 6.2   Count:
+#       words that start with a capital letter, 
+#       words written in uppercase,
+#       words written in lowercase,
+#       numbers:
+
 title_words = 0
-
-for word in text_words:
-    if word.istitle():
-        title_words += 1
-
-print(f"There are {title_words} titlecase words.")
-
-# 6.3 Count all words with uppercase letters
-upper_words = 0
-
-for word in text_words:
-    if word.isupper() and word.isalpha():
-        upper_words += 1
-
-print(f"There are {upper_words} uppercase words.")
-
-# 6.4 Count all words with lowercase letters
-lower_words = 0
-
-for word in text_words:
-    if word.islower():
-        lower_words += 1
-
-print(f"There are {lower_words} lowercase words.")
-
-# 6.5 Count all the numbers
+uppercase_words = 0
+lowercase_words = 0
 numbers = 0
 
-for word in text_words:
-    if word.isdigit():
+for word in cleaned_words:
+    if word.istitle():
+        title_words += 1
+    elif word.isupper() and word.isalpha():
+        uppercase_words += 1
+    elif word.islower():
+        lowercase_words += 1
+    elif word.isdigit():
         numbers += 1
+print(f"""There are {title_words} titlecase words.
+There are {uppercase_words} uppercase words.
+There are {lowercase_words} lowercase words.
+"There are {numbers} numeric strings.""")
 
-print(f"There are {numbers} numeric strings.")
+# 6.3 Sum of numbers
 
-# 6.6 Sum of numbers
-
-# 6.6.1 Convert each string to int:
-
+# 6.3.1 Convert each number in string format to integer format:
 numbers_int = list()
 
-for num in text_words:
+for num in cleaned_words:
     if num.isdigit():
         numbers_int.append(int(num))
 
-# 6.6.2 Sum the integers:
+# 6.3.2 Sum the integers:
 total_sum = sum(numbers_int)
 
 print(f"The sum of all the numbers {total_sum}")
@@ -114,20 +113,27 @@ print(ODDELOVAC)
 # 7. Bar graph (frequency of different word lengths in the text):
 list_of_occurences = list()
 
-for word in text_words:
+for word in cleaned_words:
     list_of_occurences.append(len(word))
 
-# 7.1 Count the occurrences of each number
-occurrences = Counter(list_of_occurences)
+dict_occurences = {}
 
-# 7.2 Sort the items by number
-sorted_occurrences = sorted(occurrences.items())
+# Cyklus přes seznam a počítání výskytů
+for number_letters in list_of_occurences:
+    if number_letters in dict_occurences:
+        dict_occurences[number_letters] += 1
+    else:
+        dict_occurences[number_letters] = 1
+
+sorted_dict_occurences = dict(sorted(dict_occurences.items()))
+
+pairs = sorted_dict_occurences.items()
 
 # 7.3 Print the header
 print(f"{'LEN':<2} | {'OCCURRENCES':<16} | {'NR.':<3}")
 print("-" * 40)
 
 # 7.4 Print each row
-for length, count in sorted_occurrences:
+for length, count in pairs:
     stars = '*' * count
     print(f"{length:<2} | {stars:<16} | {count:<3}")
